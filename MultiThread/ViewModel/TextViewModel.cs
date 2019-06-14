9ce303b2b7983f65d2c4a80bb48b4a6569f95918
nurlan3295 <nurlan3295@gmail.com>
2019-06-14 20:20:57 +0400

    Add project files.

---
+++ MultiThread/ViewModel/TextViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThread.ViewModel
+{
+  public   class TextViewModel:BaseViewModel
+   {
+        public TextViewModel()
+        {
+
+            List<string> List=new List<string>();
+            List.Add("Salam");
+            List.Add("Necesen");
+            List.Add("Sagol");
+        }
+
+
+        string currentext;
+        public string CurrentText
+        {
+            get
+            {
+                return currentext;
+            }
+            set
+            {
+
+               
+                currentext = value;
+                OnPropertyChange(new PropertyChangedEventArgs(nameof(CurrentText)));
+            }
+        }
+
+    }
+}
