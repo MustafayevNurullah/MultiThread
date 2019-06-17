6135ffce7216962d149e21f20de112a2ea8cc772
nurlan3295 <nurlan3295@gmail.com>
2019-06-17 21:57:07 +0400

    17.06.2019

--- MultiThread/ViewModel/TextViewModel.cs
+++ MultiThread/ViewModel/TextViewModel.cs
@@ -1,25 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MultiThread.ViewModel
 {
   public   class TextViewModel:BaseViewModel
    {
+
+        private List<string> allText;
+        public List<string> AllText
+        {
+            get
+            {
+                return allText;
+            }
+            set
+            {
+                allText = value;
+                OnPropertyChange(new PropertyChangedEventArgs(nameof(AllText)));
+            }
+        }
+
+        public List<string> List;
         public TextViewModel()
         {
-
-            List<string> List=new List<string>();
+            List = new List<string>();
             List.Add("Salam");
             List.Add("Necesen");
             List.Add("Sagol");
+            List.Add("Kele");
+            List.Add("Salamlar");
         }
 
-
+        public  void Process(object callback)
+        {
+            AllText = new List<string>();
+            for (int i=0;i<List.Count;i++)
+            {
+              
+            if( List[i].ToUpper().Contains(CurrentText.ToUpper()) && CurrentText!=string.Empty)
+                {
+                    AllText.Add(List[i]);
+                    
+                }
+            }
+        }
         string currentext;
         public string CurrentText
         {
@@ -29,9 +60,9 @@ namespace MultiThread.ViewModel
             }
             set
             {
-
-               
                 currentext = value;
+                ThreadPool.QueueUserWorkItem(new WaitCallback(Process));
+                
                 OnPropertyChange(new PropertyChangedEventArgs(nameof(CurrentText)));
             }
         }
